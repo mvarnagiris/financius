468868814678707e41d8d0fbe421f1a8ae8a0dfd
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2013-11-21 22:51:33 +0000

    Started doing new transaction page

--- Financius/Financius/src/main/java/com/code44/finance/ui/transactions/TransactionEditActivity.java
+++ Financius/Financius/src/main/java/com/code44/finance/ui/transactions/TransactionEditActivity.java
@@ -1,19 +1,35 @@
 package com.code44.finance.ui.transactions;
 
 import android.content.Context;
-import com.code44.finance.ui.ItemEditActivity;
-import com.code44.finance.ui.ItemEditFragment;
+import android.content.Intent;
+import android.os.Bundle;
+import com.code44.finance.R;
+import com.code44.finance.ui.AbstractActivity;
 
-public class TransactionEditActivity extends ItemEditActivity
+public class TransactionEditActivity extends AbstractActivity
 {
+    protected static final String EXTRA_ITEM_ID = TransactionEditActivity.class.getName() + ".EXTRA_ITEM_ID";
+    // -----------------------------------------------------------------------------------------------------------------
+    protected static final String FRAGMENT_ITEM = TransactionEditActivity.class.getName() + ".FRAGMENT_ITEM";
+
     public static void startItemEdit(Context context, long itemId)
     {
-        context.startActivity(makeIntent(context, TransactionEditActivity.class, itemId));
+        final Intent intent = new Intent(context, TransactionEditActivity.class);
+        context.startActivity(intent);
     }
 
     @Override
-    protected ItemEditFragment createItemEditFragment(long itemId)
+    protected void onCreate(Bundle savedInstanceState)
     {
-        return TransactionEditFragment.newInstance(itemId);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaction_edit);
+
+        // Hide ActionBar
+        //noinspection ConstantConditions
+        getActionBar().hide();
+
+        // Setup fragment
+        if (getSupportFragmentManager().findFragmentByTag(FRAGMENT_ITEM) == null)
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_V, TransactionEditFragment.newInstance(0), FRAGMENT_ITEM).commit();
     }
 }
