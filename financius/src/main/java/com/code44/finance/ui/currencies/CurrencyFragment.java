cecb6c5fecc3a6e44814b17140ac2ef15cc61546
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-17 15:08:57 +0100

    Currency UI that I'm finally happy about

--- financius/src/main/java/com/code44/finance/ui/currencies/CurrencyFragment.java
+++ financius/src/main/java/com/code44/finance/ui/currencies/CurrencyFragment.java
@@ -3,6 +3,7 @@ package com.code44.finance.ui.currencies;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.Loader;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
@@ -12,12 +13,19 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.code44.finance.R;
 import com.code44.finance.api.currencies.CurrenciesAsyncApi;
 import com.code44.finance.api.currencies.CurrencyRequest;
+import com.code44.finance.data.Query;
+import com.code44.finance.data.db.Tables;
+import com.code44.finance.data.db.model.Account;
+import com.code44.finance.data.db.model.BaseModel;
 import com.code44.finance.data.db.model.Currency;
+import com.code44.finance.data.providers.AccountsProvider;
 import com.code44.finance.data.providers.CurrenciesProvider;
 import com.code44.finance.ui.ModelFragment;
 import com.code44.finance.utils.MoneyFormatter;
@@ -26,11 +34,15 @@ import de.greenrobot.event.EventBus;
 import fr.castorflex.android.smoothprogressbar.SmoothProgressBar;
 
 public class CurrencyFragment extends ModelFragment<Currency> {
+    private static final int LOADER_ACCOUNTS = 1;
+
     private SmoothProgressBar loading_SPB;
     private TextView code_TV;
     private TextView format_TV;
-    private TextView mainFormat_TV;
     private TextView exchangeRate_TV;
+    private LinearLayout accountsContainer_V;
+
+    private int accountsContainerStaticViewCount;
 
     public static CurrencyFragment newInstance(long currencyId) {
         final Bundle args = makeArgs(currencyId);
@@ -59,8 +71,11 @@ public class CurrencyFragment extends ModelFragment<Currency> {
         loading_SPB = (SmoothProgressBar) view.findViewById(R.id.loading_SPB);
         code_TV = (TextView) view.findViewById(R.id.code_TV);
         format_TV = (TextView) view.findViewById(R.id.format_TV);
-        mainFormat_TV = (TextView) view.findViewById(R.id.mainFormat_TV);
         exchangeRate_TV = (TextView) view.findViewById(R.id.exchangeRate_TV);
+        accountsContainer_V = (LinearLayout) view.findViewById(R.id.accountsContainer_V);
+
+        // Setup
+        accountsContainerStaticViewCount = accountsContainer_V.getChildCount();
     }
 
     @Override
@@ -101,6 +116,30 @@ public class CurrencyFragment extends ModelFragment<Currency> {
     }
 
     @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (id == LOADER_ACCOUNTS) {
+            return Query.get()
+                    .projectionId(Tables.Accounts.ID)
+                    .projection(Tables.Accounts.PROJECTION)
+                    .projection(Tables.Currencies.PROJECTION)
+                    .selection(Tables.Accounts.OWNER + "=?", String.valueOf(Account.Owner.USER.asInt()))
+                    .selection(" and " + Tables.Accounts.ITEM_STATE + "=?", String.valueOf(BaseModel.ItemState.NORMAL.asInt()))
+                    .asCursorLoader(getActivity(), AccountsProvider.uriAccounts());
+        }
+
+        return super.onCreateLoader(id, args);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (loader.getId() == LOADER_ACCOUNTS) {
+            onAccountsLoaded(data);
+            return;
+        }
+        super.onLoadFinished(loader, data);
+    }
+
+    @Override
     protected Uri getUri(long modelId) {
         return CurrenciesProvider.uriCurrency(modelId);
     }
@@ -117,17 +156,17 @@ public class CurrencyFragment extends ModelFragment<Currency> {
             code_TV.setText(currency.getCode());
             code_TV.setTextColor(getResources().getColor(R.color.text_accent));
             exchangeRate_TV.setText(R.string.main_currency);
-            mainFormat_TV.setVisibility(View.GONE);
         } else {
             final SpannableStringBuilder ssb = new SpannableStringBuilder(currency.getCode() + " \u2192 " + Currency.getDefault().getCode());
-            ssb.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.text_accent)), ssb.length() - 3, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+            ssb.setSpan(new ForegroundColorSpan(getResources().getColor(R.color.text_secondary)), ssb.length() - 3, ssb.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
             code_TV.setText(ssb);
             code_TV.setTextColor(getResources().getColor(R.color.text_primary));
             exchangeRate_TV.setText(String.valueOf(currency.getExchangeRate()));
-            mainFormat_TV.setText(MoneyFormatter.format(Currency.getDefault(), (long) (100000 * currency.getExchangeRate())));
-            mainFormat_TV.setVisibility(View.VISIBLE);
         }
         format_TV.setText(MoneyFormatter.format(currency, 100000));
+
+        // Loader
+        getLoaderManager().restartLoader(LOADER_ACCOUNTS, null, this);
     }
 
     @SuppressWarnings("UnusedDeclaration")
@@ -141,5 +180,33 @@ public class CurrencyFragment extends ModelFragment<Currency> {
 
     private void refreshRate() {
         CurrenciesAsyncApi.get().updateExchangeRate(model.getCode());
+    }
+
+    private void onAccountsLoaded(Cursor cursor) {
+        if (accountsContainer_V.getChildCount() > accountsContainerStaticViewCount) {
+            accountsContainer_V.removeViews(accountsContainerStaticViewCount, accountsContainer_V.getChildCount() - accountsContainerStaticViewCount);
+        }
+
+        final LayoutInflater inflater = LayoutInflater.from(getActivity());
+        if (cursor.moveToFirst()) {
+            do {
+                final Account account = Account.from(cursor);
+                final View view = inflater.inflate(R.layout.li_currency_account, accountsContainer_V, false);
+                ((TextView) view.findViewById(R.id.title_TV)).setText(account.getTitle() + ", " + account.getCurrency().getCode());
+                final Button currency_B = (Button) view.findViewById(R.id.currency_B);
+                if (account.getCurrency().getId() != model.getId()) {
+                    currency_B.setText(getString(R.string.f_change_to_x, model.getCode()));
+                    currency_B.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // TODO Update account currency
+                        }
+                    });
+                } else {
+                    currency_B.setVisibility(View.GONE);
+                }
+                accountsContainer_V.addView(view);
+            } while (cursor.moveToNext());
+        }
     }
 }
