e6261dc324984c83b77f112b3c493cf7c6a3147c
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-20 17:14:24 +0100

    Tests placeholders for TransactionsProvider

---
+++ financius/src/test/java/com/code44/finance/data/providers/TransactionsProviderTest.java
@@ -0,0 +1,30 @@
+package com.code44.finance.data.providers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class TransactionsProviderTest extends BaseContentProviderTestCase {
+    @Test
+    public void insert_updatesAccountsBalances() {
+        fail();
+    }
+
+    @Test
+    public void update_updatesAccountsBalances() {
+        fail();
+    }
+
+    @Test
+    public void delete_updatesAccountsBalances() {
+        fail();
+    }
+
+    @Test
+    public void bulkInsert_updatesAccountsBalances() {
+        fail();
+    }
+}
