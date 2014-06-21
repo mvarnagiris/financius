164722d7c6ab1e4c5aa16e6325f5a89d69e52d77
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-21 12:58:30 +0100

    account balance update WIP

--- financius/src/main/java/com/code44/finance/data/providers/TransactionsProvider.java
+++ financius/src/main/java/com/code44/finance/data/providers/TransactionsProvider.java
@@ -1,9 +1,18 @@
 package com.code44.finance.data.providers;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
 
+import com.code44.finance.data.Query;
 import com.code44.finance.data.db.Tables;
+import com.code44.finance.data.db.model.BaseModel;
+import com.code44.finance.data.db.model.Category;
+import com.code44.finance.data.db.model.Transaction;
+import com.code44.finance.utils.IOUtils;
+
+import java.util.Map;
 
 public class TransactionsProvider extends BaseModelProvider {
     public static Uri uriTransactions() {
@@ -27,5 +36,53 @@ public class TransactionsProvider extends BaseModelProvider {
                 + " inner join " + Tables.Accounts.TABLE_NAME + " as " + Tables.Accounts.TEMP_TABLE_NAME_TO_ACCOUNT
                 + " on " + Tables.Accounts.TEMP_TABLE_NAME_TO_ACCOUNT + "." + BaseColumns._ID + "=" + Tables.Transactions.ACCOUNT_TO_ID
                 + " inner join " + Tables.Categories.TABLE_NAME + " on " + Tables.Categories.ID.getNameWithTable() + "=" + Tables.Transactions.CATEGORY_ID;
+    }
+
+    @Override
+    protected void onAfterInsertItem(Uri uri, ContentValues values, long id, Map<String, Object> extras) {
+        super.onAfterInsertItem(uri, values, id, extras);
+
+        final Cursor cursor = Query.get()
+                .projectionId(Tables.Transactions.ID)
+                .projection(Tables.Transactions.PROJECTION)
+                .projection(Tables.Accounts.PROJECTION_ACCOUNT_FROM)
+                .projection(Tables.Accounts.PROJECTION_ACCOUNT_TO)
+                .projection(Tables.Categories.PROJECTION)
+                .asCursor(getContext(), uriTransaction(id));
+        final Transaction transaction = Transaction.from(cursor);
+        IOUtils.closeQuietly(cursor);
+
+        if (transaction.getCategory().getType() == Category.Type.EXPENSE) {
+            updateAccountBalance(transaction.getAccountFrom().getId());
+        } else if (transaction.getCategory().getType() == Category.Type.INCOME) {
+            updateAccountBalance(transaction.getAccountTo().getId());
+        } else {
+            updateAccountBalance(transaction.getAccountFrom().getId());
+            updateAccountBalance(transaction.getAccountTo().getId());
+        }
+    }
+
+    @Override
+    protected void onBeforeUpdateItems(Uri uri, ContentValues values, String selection, String[] selectionArgs, Map<String, Object> outExtras) {
+        throw new IllegalArgumentException("Update is not supported.");
+    }
+
+    @Override
+    protected void onAfterDeleteItems(Uri uri, String selection, String[] selectionArgs, BaseModel.ItemState itemState, Map<String, Object> extras) {
+        super.onAfterDeleteItems(uri, selection, selectionArgs, itemState, extras);
+        updateAccountsBalance(selection, selectionArgs);
+    }
+
+    @Override
+    protected void onAfterBulkInsertItems(Uri uri, ContentValues[] valuesArray, Map<String, Object> extras) {
+        super.onAfterBulkInsertItems(uri, valuesArray, extras);
+        updateAccountsBalance(null);
+    }
+
+    private void updateAccountBalance(long accountId) {
+        updateAccountsBalance(Tables.Transactions.ID.getNameWithTable() + "=?", String.valueOf(transactionId));
+    }
+
+    private void updateAccountsBalance(String selection, String... selectionArgs) {
     }
 }
--- financius/src/test/java/com/code44/finance/data/providers/TransactionsProviderTest.java
+++ financius/src/test/java/com/code44/finance/data/providers/TransactionsProviderTest.java
@@ -1,5 +1,15 @@
 package com.code44.finance.data.providers;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+
+import com.code44.finance.data.Query;
+import com.code44.finance.data.db.Tables;
+import com.code44.finance.data.db.model.Account;
+import com.code44.finance.data.db.model.Category;
+import com.code44.finance.data.db.model.Transaction;
+import com.code44.finance.utils.IOUtils;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.RobolectricTestRunner;
@@ -10,21 +20,64 @@ import static org.junit.Assert.*;
 public class TransactionsProviderTest extends BaseContentProviderTestCase {
     @Test
     public void insert_updatesAccountsBalances() {
-        fail();
+        final Transaction transaction = new Transaction();
+        transaction.setAccountFrom(insertAccount());
+        transaction.setCategory(Category.getExpense());
+        transaction.setAmount(42);
+
+        insert(TransactionsProvider.uriTransactions(), transaction);
+        final Account account = getAccount();
+
+        assertEquals(42, account.getBalance());
     }
 
-    @Test
-    public void update_updatesAccountsBalances() {
-        fail();
+    @Test(expected = IllegalArgumentException.class)
+    public void update_throwsIllegalArgumentException() {
+        update(TransactionsProvider.uriTransactions(), new ContentValues(), null);
     }
 
     @Test
     public void delete_updatesAccountsBalances() {
-        fail();
+        final Transaction transaction = new Transaction();
+        transaction.setAccountFrom(insertAccount());
+        transaction.setCategory(Category.getExpense());
+        transaction.setAmount(42);
+
+        insert(TransactionsProvider.uriTransactions(), transaction);
+        delete(TransactionsProvider.uriTransactions(), null);
+        final Account account = getAccount();
+
+        assertEquals(0, account.getBalance());
     }
 
     @Test
     public void bulkInsert_updatesAccountsBalances() {
-        fail();
+        final Transaction transaction = new Transaction();
+        transaction.setAccountFrom(insertAccount());
+        transaction.setCategory(Category.getExpense());
+        transaction.setAmount(42);
+
+        contentResolver.bulkInsert(TransactionsProvider.uriTransactions(), new ContentValues[]{transaction.asContentValues()});
+        final Account account = getAccount();
+
+        assertEquals(42, account.getBalance());
+    }
+
+    private Account insertAccount() {
+        final Account account = new Account();
+        account.setTitle("a");
+        account.setId(insert(AccountsProvider.uriAccounts(), account));
+        return account;
+    }
+
+    private Account getAccount() {
+        final Cursor cursor = Query.get()
+                .projectionId(Tables.Accounts.ID)
+                .projection(Tables.Accounts.PROJECTION)
+                .projection(Tables.Currencies.PROJECTION)
+                .asCursor(context, AccountsProvider.uriAccounts());
+        final Account account = Account.from(cursor);
+        IOUtils.closeQuietly(cursor);
+        return account;
     }
 }
