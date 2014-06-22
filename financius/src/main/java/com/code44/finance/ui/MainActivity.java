74ab27ccf7cdf7dca94629b2b435c14be44e8338
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-22 14:34:48 +0100

    Removed some debug stuff from MainActivity

--- financius/src/main/java/com/code44/finance/ui/MainActivity.java
+++ financius/src/main/java/com/code44/finance/ui/MainActivity.java
@@ -1,12 +1,6 @@
 package com.code44.finance.ui;
 
-import android.database.Cursor;
 import android.os.Bundle;
-
-import com.code44.finance.data.Query;
-import com.code44.finance.data.db.Tables;
-import com.code44.finance.data.providers.TransactionsProvider;
-import com.code44.finance.utils.IOUtils;
 
 
 public class MainActivity extends BaseActivity {
@@ -17,12 +11,5 @@ public class MainActivity extends BaseActivity {
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().add(android.R.id.content, OverviewFragment.newInstance()).commit();
         }
-
-        Cursor cursor = Query.get().projectionId(Tables.Transactions.ID)
-                .projection(Tables.Transactions.PROJECTION)
-                .projection(Tables.Categories.PROJECTION).from(this, TransactionsProvider.uriTransactions()).execute();
-        cursor.moveToFirst();
-        int count = cursor.getCount();
-        IOUtils.closeQuietly(cursor);
     }
 }
