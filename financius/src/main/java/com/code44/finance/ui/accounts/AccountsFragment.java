74e113db1104ccfcc92b39192b912c30049e25ba
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-24 08:44:07 +0100

    Added account balance to account list

--- financius/src/main/java/com/code44/finance/ui/accounts/AccountsFragment.java
+++ financius/src/main/java/com/code44/finance/ui/accounts/AccountsFragment.java
@@ -4,9 +4,11 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.code44.finance.R;
 import com.code44.finance.adapters.AccountsAdapter;
@@ -15,10 +17,14 @@ import com.code44.finance.data.Query;
 import com.code44.finance.data.db.Tables;
 import com.code44.finance.data.db.model.Account;
 import com.code44.finance.data.db.model.BaseModel;
+import com.code44.finance.data.db.model.Currency;
 import com.code44.finance.data.providers.AccountsProvider;
 import com.code44.finance.ui.ModelListFragment;
+import com.code44.finance.utils.MoneyFormatter;
 
 public class AccountsFragment extends ModelListFragment {
+    private TextView balance_TV;
+
     public static AccountsFragment newInstance(int mode) {
         final Bundle args = makeArgs(mode);
 
@@ -30,6 +36,22 @@ public class AccountsFragment extends ModelListFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_accounts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Get views
+        balance_TV = (TextView) view.findViewById(R.id.balance_TV);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (loader.getId() == LOADER_MODELS) {
+            updateBalance(data);
+        }
+        super.onLoadFinished(loader, data);
     }
 
     @Override
@@ -65,5 +87,15 @@ public class AccountsFragment extends ModelListFragment {
                 .projection(Tables.Currencies.PROJECTION)
                 .selection(Tables.Accounts.OWNER + "<>?")
                 .args(Account.Owner.SYSTEM.asString());
+    }
+
+    private void updateBalance(Cursor cursor) {
+        long balance = 0;
+        if (cursor.moveToFirst()) {
+            do {
+                balance += Account.from(cursor).getBalance();
+            } while (cursor.moveToNext());
+        }
+        balance_TV.setText(MoneyFormatter.format(Currency.getDefault(), balance));
     }
 }
