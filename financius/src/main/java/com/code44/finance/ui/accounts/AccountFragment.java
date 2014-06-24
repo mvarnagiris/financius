45beeec55fdd09a2165f390aaa1cfe4b7d4a8bf5
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-06-24 08:55:19 +0100

    Account view WIP

--- financius/src/main/java/com/code44/finance/ui/accounts/AccountFragment.java
+++ financius/src/main/java/com/code44/finance/ui/accounts/AccountFragment.java
@@ -3,6 +3,9 @@ package com.code44.finance.ui.accounts;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.code44.finance.data.db.model.Account;
 import com.code44.finance.data.providers.AccountsProvider;
@@ -15,6 +18,11 @@ public class AccountFragment extends ModelFragment<Account> {
         final AccountFragment fragment = new AccountFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
     }
 
     @Override
