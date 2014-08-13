c198130f4aa0029950ef59b92b3f5b563b921368
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-08-13 18:07:37 +0100

    Transactions list updates

--- financius/src/main/java/com/code44/finance/adapters/TransactionsAdapter.java
+++ financius/src/main/java/com/code44/finance/adapters/TransactionsAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import com.code44.finance.R;
 import com.code44.finance.common.model.CategoryType;
 import com.code44.finance.data.db.model.Category;
+import com.code44.finance.data.db.model.Currency;
 import com.code44.finance.data.db.model.Transaction;
 import com.code44.finance.utils.MoneyFormatter;
 
@@ -40,40 +41,57 @@ public class TransactionsAdapter extends BaseModelsAdapter {
         final ViewHolder holder = (ViewHolder) view.getTag();
         final Transaction transaction = Transaction.from(cursor);
         final Category category = transaction.getCategory();
+        final DateTime date = new DateTime(transaction.getDate());
         holder.color_IV.setColorFilter(category.getColor());
-        holder.day_TV.setText(String.valueOf(new DateTime(transaction.getDate()).dayOfMonth().get()));
+        holder.weekday_TV.setText(date.dayOfWeek().getAsShortText());
+        holder.day_TV.setText(date.dayOfMonth().getAsShortText());
         holder.category_TV.setText(category.getTitle());
         holder.note_TV.setText(transaction.getNote());
         holder.amount_TV.setText(MoneyFormatter.format(transaction));
+
+        final Currency currency;
         if (category.getCategoryType() == CategoryType.EXPENSE) {
             holder.account_TV.setText(transaction.getAccountFrom().getTitle());
             holder.amount_TV.setTextColor(expenseColor);
+            currency = transaction.getAccountFrom().getCurrency();
         } else if (category.getCategoryType() == CategoryType.INCOME) {
             holder.account_TV.setText(transaction.getAccountTo().getTitle());
             holder.amount_TV.setTextColor(incomeColor);
+            currency = transaction.getAccountTo().getCurrency();
         } else {
             holder.account_TV.setText(transaction.getAccountFrom().getTitle() + " > " + transaction.getAccountTo().getTitle());
             holder.amount_TV.setTextColor(transferColor);
+            currency = transaction.getAccountFrom().getCurrency();
         }
 
+        if (currency.isDefault()) {
+            holder.amountDefault_TV.setVisibility(View.GONE);
+        } else {
+            holder.amountDefault_TV.setVisibility(View.VISIBLE);
+            holder.amountDefault_TV.setText(MoneyFormatter.format(Currency.getDefault(), (long) (transaction.getAmount() * currency.getExchangeRate())));
+        }
     }
 
     private static class ViewHolder {
         public ImageView color_IV;
+        public TextView weekday_TV;
         public TextView day_TV;
         public TextView category_TV;
         public TextView note_TV;
         public TextView amount_TV;
         public TextView account_TV;
+        public TextView amountDefault_TV;
 
         public static ViewHolder setAsTag(View view) {
             final ViewHolder holder = new ViewHolder();
             holder.color_IV = (ImageView) view.findViewById(R.id.color_IV);
+            holder.weekday_TV = (TextView) view.findViewById(R.id.weekday_TV);
             holder.day_TV = (TextView) view.findViewById(R.id.day_TV);
             holder.category_TV = (TextView) view.findViewById(R.id.category_TV);
             holder.note_TV = (TextView) view.findViewById(R.id.note_TV);
             holder.amount_TV = (TextView) view.findViewById(R.id.amount_TV);
             holder.account_TV = (TextView) view.findViewById(R.id.account_TV);
+            holder.amountDefault_TV = (TextView) view.findViewById(R.id.amountDefault_TV);
             view.setTag(holder);
 
             return holder;
