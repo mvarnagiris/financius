7024e40e0d302989bbcbd5d55c9e04598e4ed720
Mantas Varnagiris <mantas.varnagiris@gmail.com>
2014-11-08 14:48:48 +0000

    Added Crashlytics

--- financius/src/main/java/com/code44/finance/ui/SplashActivity.java
+++ financius/src/main/java/com/code44/finance/ui/SplashActivity.java
@@ -6,6 +6,7 @@ import com.code44.finance.BuildConfig;
 import com.code44.finance.ui.common.BaseActivity;
 import com.code44.finance.ui.overview.OverviewActivity;
 import com.code44.finance.utils.GeneralPrefs;
+import com.crashlytics.android.Crashlytics;
 
 import javax.inject.Inject;
 
@@ -14,6 +15,7 @@ public class SplashActivity extends BaseActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Crashlytics.start(this);
 
         checkVersionUpdate();
 
