bdd99f4751eea37390e9b71a8d9eca8bf10715c0
Mantas <mantas.varnagiris@gmail.com>
2014-12-01 10:30:25 +0000

    Note autocomplete is working

--- financius/src/main/java/com/code44/finance/ui/transactions/NoteController.java
+++ financius/src/main/java/com/code44/finance/ui/transactions/NoteController.java
@@ -4,10 +4,12 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 
 import com.code44.finance.common.utils.StringUtils;
@@ -16,18 +18,21 @@ import com.code44.finance.data.db.Tables;
 import com.code44.finance.data.providers.TransactionsProvider;
 import com.code44.finance.ui.common.BaseActivity;
 
-public class NoteController implements LoaderManager.LoaderCallbacks<Cursor>, TextWatcher {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NoteController implements LoaderManager.LoaderCallbacks<Cursor>, TextWatcher, AdapterView.OnItemClickListener {
     private static final int LOADER_NOTES = 8125;
 
     private final AutoCompleteTextView noteAutoCompleteTextView;
-    private final SimpleCursorAdapter adapter;
 
     public NoteController(AutoCompleteTextView noteAutoCompleteTextView) {
         this.noteAutoCompleteTextView = noteAutoCompleteTextView;
 
-        this.adapter = new SimpleCursorAdapter(noteAutoCompleteTextView.getContext(), android.R.layout.simple_dropdown_item_1line, null, new String[]{Tables.Transactions.NOTE.getName()}, new int[]{android.R.id.text1}, 0);
-        this.noteAutoCompleteTextView.setAdapter(adapter);
         this.noteAutoCompleteTextView.addTextChangedListener(this);
+        this.noteAutoCompleteTextView.setOnItemClickListener(this);
+        setAutoCompleteAdapter(Collections.<String>emptyList());
 
         ((BaseActivity) this.noteAutoCompleteTextView.getContext()).getSupportLoaderManager().initLoader(LOADER_NOTES, null, this);
     }
@@ -49,13 +54,17 @@ public class NoteController implements LoaderManager.LoaderCallbacks<Cursor>, Te
     }
 
     @Override public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        if (!data.isClosed()) {
-            adapter.swapCursor(data);
+        final List<String> items = new ArrayList<>();
+        if (data != null && data.moveToFirst()) {
+            final int noteIndex = data.getColumnIndex(Tables.Transactions.NOTE.getName());
+            do {
+                items.add(data.getString(noteIndex));
+            } while (data.moveToNext());
         }
+        setAutoCompleteAdapter(items);
     }
 
     @Override public void onLoaderReset(Loader<Cursor> loader) {
-        adapter.swapCursor(null);
     }
 
     @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -68,11 +77,22 @@ public class NoteController implements LoaderManager.LoaderCallbacks<Cursor>, Te
     @Override public void afterTextChanged(Editable s) {
     }
 
+    @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        final String value = (String) parent.getItemAtPosition(position);
+        setNote(value);
+        noteAutoCompleteTextView.setSelection(noteAutoCompleteTextView.getText().length());
+    }
+
     public String getNote() {
         return noteAutoCompleteTextView.getText().toString();
     }
 
     public void setNote(String note) {
         noteAutoCompleteTextView.setText(note);
+    }
+
+    private void setAutoCompleteAdapter(List<String> values) {
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(noteAutoCompleteTextView.getContext(), android.R.layout.simple_dropdown_item_1line, values);
+        this.noteAutoCompleteTextView.setAdapter(adapter);
     }
 }
