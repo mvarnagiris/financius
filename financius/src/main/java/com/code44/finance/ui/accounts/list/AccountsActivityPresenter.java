b508e606243294ad6fc461db84ccaa2941027c64
Mantas <mantas.varnagiris@gmail.com>
2015-01-12 17:20:27 +0000

    Showing balance on top

--- financius/src/main/java/com/code44/finance/ui/accounts/list/AccountsActivityPresenter.java
+++ financius/src/main/java/com/code44/finance/ui/accounts/list/AccountsActivityPresenter.java
@@ -2,13 +2,9 @@ package com.code44.finance.ui.accounts.list;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Canvas;
-import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,7 +19,6 @@ import com.code44.finance.ui.common.activities.BaseActivity;
 import com.code44.finance.ui.common.adapters.ModelsAdapter;
 import com.code44.finance.ui.common.presenters.ModelsActivityPresenter;
 import com.code44.finance.utils.MoneyFormatter;
-import com.code44.finance.utils.ThemeUtils;
 
 public class AccountsActivityPresenter extends ModelsActivityPresenter<Account> {
     private final Currency mainCurrency;
@@ -34,14 +29,15 @@ public class AccountsActivityPresenter extends ModelsActivityPresenter<Account> 
         this.mainCurrency = mainCurrency;
     }
 
-    @Override protected void setupRecyclerView(RecyclerView recyclerView) {
-        super.setupRecyclerView(recyclerView);
-        recyclerView.addItemDecoration(new ThickDividerDecoration(recyclerView.getContext()));
-    }
-
     @Override public void onActivityCreated(BaseActivity activity, Bundle savedInstanceState) {
         super.onActivityCreated(activity, savedInstanceState);
+
+        final View balanceContainerView = findView(activity, R.id.balanceContainerView);
         balanceTextView = findView(activity, R.id.balanceTextView);
+
+        if (getMode() != Mode.View) {
+            balanceContainerView.setVisibility(View.GONE);
+        }
     }
 
     @Override public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
@@ -78,60 +74,5 @@ public class AccountsActivityPresenter extends ModelsActivityPresenter<Account> 
             } while (cursor.moveToNext());
         }
         balanceTextView.setText(MoneyFormatter.format(mainCurrency, balance));
-    }
-
-    private static class ThickDividerDecoration extends RecyclerView.ItemDecoration {
-        private final Drawable dividerDrawable;
-
-        private ThickDividerDecoration(Context context) {
-            dividerDrawable = ThemeUtils.getDrawable(context, android.R.attr.dividerHorizontal);
-        }
-
-        @Override public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
-            super.onDrawOver(c, parent, state);
-
-            final RecyclerView.LayoutManager layoutManager = parent.getLayoutManager();
-            for (int i = 0, size = parent.getChildCount(); i < size; i++) {
-                final View view = parent.getChildAt(i);
-                if (!drawDivider(view, parent)) {
-                    continue;
-                }
-
-                final int left = layoutManager.getDecoratedLeft(view);
-                final int right = layoutManager.getDecoratedRight(view);
-                final int top = layoutManager.getDecoratedTop(view);
-                final int bottom = top + (dividerDrawable.getIntrinsicHeight() * 2);
-
-                dividerDrawable.setBounds(left, top, right, bottom);
-                dividerDrawable.draw(c);
-            }
-        }
-
-        @Override public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
-            super.getItemOffsets(outRect, view, parent, state);
-
-            if (!drawDivider(view, parent)) {
-                return;
-            }
-
-            outRect.top += dividerDrawable.getIntrinsicHeight() * 2;
-        }
-
-        private boolean drawDivider(View view, RecyclerView parent) {
-            if (((AccountsAdapter.ViewHolder) parent.getChildViewHolder(view)).getModel().includeInTotals()) {
-                return false;
-            }
-
-            final int previousViewIndex = parent.indexOfChild(view) - 1;
-            if (previousViewIndex < 0) {
-                return false;
-            }
-
-            if (!((AccountsAdapter.ViewHolder) parent.getChildViewHolder(parent.getChildAt(previousViewIndex))).getModel().includeInTotals()) {
-                return false;
-            }
-
-            return true;
-        }
     }
 }
